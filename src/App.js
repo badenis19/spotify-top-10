63006ec38a7b62bcacf35bc658d5557662655a0f
Bruno ADENIS <adenis.bruno@hotmail.com>
2020-01-12 21:01:50 +0000

    use of react route, path create and accessible

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import SearchBar from "./SearchBar"
+import Result from "./Result";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // to handle routing
 
 function App() {
   return (
-    <div className="App">
-      <h1>HOME PAGE</h1>
-      < SearchBar />
-    </div>
+    <Router>
+      <div className="App">
+        < Route 
+          path="/result" 
+          component={Result} 
+       />
+
+        < Route path="/"
+            component={SearchBar}
+            exact
+        />
+      </div>
+
+    </Router>
   );
 }
 
